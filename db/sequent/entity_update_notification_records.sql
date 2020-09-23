433f5282fec70235152c0f428d52bd953dedcf2b
Trey Evans <tevans@justplainawful.lan>
2020-09-23 14:38:11 -0400

    Adding first table for uncategorized entity payloads.

--- db/sequent/entity_update_notification_records.sql
+++ db/sequent/entity_update_notification_records.sql
@@ -1,6 +1,7 @@
 CREATE TABLE entity_update_notifications%SUFFIX% (
     id BIGSERIAL PRIMARY KEY,
-    aggregate_id uuid NOT NULL
+    aggregate_id uuid NOT NULL,
+    message_body bytea
 );
 
 CREATE UNIQUE INDEX unique_aggregate_id%SUFFIX% ON entity_update_notifications%SUFFIX% USING btree (aggregate_id);
